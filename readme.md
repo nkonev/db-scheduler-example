852143cf4765565d766e14f12dda3615a365068d
Nikita Konev <nikit.cpp@yandex.ru>
2020-12-30 08:07:04 +0300

    add retrying with attempts

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@ curl -i 'http://localhost:8080/task'
 
 curl -i -X PUT 'http://localhost:8080/task?afterSeconds=5&data=100500'
 curl -i -X PUT 'http://localhost:8080/task?afterSeconds=5&data=100501'
+curl -i -X PUT 'http://localhost:8080/limited?afterSeconds=5&data=-42'
 ```
 
 Open the database
--- src/main/kotlin/name/nkonev/dbscheduler/dbscheduler/DbSchedulerApplication.kt
+++ src/main/kotlin/name/nkonev/dbscheduler/dbscheduler/DbSchedulerApplication.kt
@@ -16,6 +16,7 @@ import com.github.kagkarlsson.scheduler.task.TaskInstance
 import com.github.kagkarlsson.scheduler.task.helper.OneTimeTask
 import com.github.kagkarlsson.scheduler.task.helper.Tasks
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -28,10 +29,12 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Clock
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import java.util.Optional
 import java.util.UUID
 
 data class MyTaskData(var id: Long = 0)
+data class MyFailingTaskData(var id: Long = 0, var failureCounter: Long = 0)
 
 @SpringBootApplication
 class DbSchedulerApplication : CommandLineRunner {
@@ -55,18 +58,45 @@ class ClockConfiguration {
 	fun clock(): Clock = Clock.systemUTC()
 }
 
+const val DATA_MOVER_TASK_NAME = "dataMoverTask"
+const val LIMITED_TASK_NAME = "limitedTask"
+
 @Configuration
 class DbSchedulerConfig (private val clock: Clock) {
 
-	@Bean
+	@Bean(name = [DATA_MOVER_TASK_NAME])
 	fun dataMoverTask() : OneTimeTask<MyTaskData> {
 		val myAdhocTask: OneTimeTask<MyTaskData> = Tasks.oneTime("data-mover-task", MyTaskData::class.java)
 				.execute {   inst: TaskInstance<MyTaskData>, ctx: ExecutionContext? ->
-					System.out.println("Executed! Custom data, Id: " + inst.getData().id + " at " + clock.instant())
+					System.out.println("Executing dataMoverTask! Custom data, Id: " + inst.getData().id + " at " + clock.instant())
 
 				}
 		return myAdhocTask
 	}
+
+	@Bean(name = [LIMITED_TASK_NAME])
+	fun failLimitedTask() : OneTimeTask<MyFailingTaskData> {
+		val myAdhocTask: OneTimeTask<MyFailingTaskData> = Tasks.oneTime("fail-limited-task", MyFailingTaskData::class.java)
+				.onFailure { executionComplete, executionOperations ->
+					if (executionComplete.cause?.get() is RuntimeException) {
+						val d: MyFailingTaskData = executionComplete.execution.taskInstance.data as MyFailingTaskData
+						val newD = d.copy(failureCounter = d.failureCounter+1)
+						if (d.failureCounter < 4) {
+							executionOperations.reschedule(executionComplete, Instant.now().plus(2, ChronoUnit.SECONDS), newD)
+						} else{
+							System.out.println("Giving up on failLimitedTask! Custom data, Id: " + d.id + " at " + clock.instant())
+						}
+					}
+				}
+				.execute {   inst: TaskInstance<MyFailingTaskData>, ctx: ExecutionContext? ->
+					System.out.println("Executing failLimitedTask! Custom data, Id: " + inst.getData().id + " at " + clock.instant())
+					if (inst.getData().id == -42L) {
+						throw RuntimeException("Unable to process this number")
+					}
+				}
+		return myAdhocTask
+	}
+
 
 	@Bean
 	fun dbSchedulerCustomizer() : DbSchedulerCustomizer {
@@ -109,8 +139,13 @@ class MyController {
 	@Autowired
 	private lateinit var dbScheduler: Scheduler
 
+	@Qualifier(DATA_MOVER_TASK_NAME)
 	@Autowired
 	private lateinit var dataMoverTask: OneTimeTask<MyTaskData>
+
+	@Qualifier(LIMITED_TASK_NAME)
+	@Autowired
+	private lateinit var limitedTask: OneTimeTask<MyFailingTaskData>
 
 	@GetMapping("/task")
 	fun getSomething() : List<CurrentlyExecuting> {
@@ -118,8 +153,13 @@ class MyController {
 	}
 
 	@PutMapping("/task")
-	fun addSomething(@RequestParam data: Long, @RequestParam afterSeconds: Long) {
+	fun addDataMoverTask(@RequestParam data: Long, @RequestParam afterSeconds: Long) {
 		dbScheduler.schedule(dataMoverTask.instance(UUID.randomUUID().toString(), MyTaskData(data)), Instant.now().plusSeconds(afterSeconds));
+	}
+
+	@PutMapping("/limited")
+	fun addSLimitedTask(@RequestParam data: Long, @RequestParam afterSeconds: Long) {
+		dbScheduler.schedule(limitedTask.instance(UUID.randomUUID().toString(), MyFailingTaskData(data)), Instant.now().plusSeconds(afterSeconds));
 	}
 
 }
